34ea44910facb7f14b9ba590368094719037e359
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-11-11 18:46:43 -0500

    add in select template functionality

--- react-template/src/App.js
+++ react-template/src/App.js
@@ -1,14 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as request from 'superagent';
+
+const templateRepo = 'acq-templates';
+const remoteBranch = 'develop';
 
 class SelectTemplate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {repoForTemplates: templateRepo,
+                  branchForTemplates: remoteBranch,
+                  availableTemplateDirectories: [],
+                  availableTemplateFiles: [],
+                  templateDirectorySelected: 'false',
+                  templateSelected: "",
+                };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSub = this.handleChangeSub.bind(this);
+  }
+  componentDidMount() {
+    this.getGitFiles();
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  getGitFiles(){
+    const url = 'https://api.github.com/repos/18F/'+this.state.repoForTemplates+'/git/trees/'+this.state.branchForTemplates+'?recursive=1';
+    const self = this;
+    request
+    .get(url)
+    .set('Accept', 'application/json')
+    .end(function(err, res){
+      if(err){
+        console.log(err.toString());
+      }
+      if(res){
+        const parsedData = JSON.parse(res.text);
+        const directories = parsedData.tree.filter(gitPath => gitPath.type === 'tree');
+        const subFiles = parsedData.tree.filter((file) => {
+            return file.path.split('/').length > 1 && file.path.split('.').pop() === 'md';
+        });
+        let subFileHash = {};
+        subFiles.forEach(file => {
+          const pathArray = file.path.split('/');
+          file.filename = pathArray.pop();
+          if (pathArray[0] in subFileHash){
+            subFileHash[pathArray[0]].push(file);
+          } else {
+          subFileHash[pathArray[0]] = [file];
+          }
+        });
+        self.setState({availableTemplateDirectories: directories,
+                      availableTemplateFiles: subFileHash});
+      }
+    });
+  }
+
+  renderListItem(items){
+    const selectors = [];
+    selectors.push(<option key='false' value='false'>Select a template type</option>);
+    items.forEach(item => {
+      selectors.push(<option key={item.path} value={item.path}>{item.path}</option>)
+    })
+    return selectors;
+  }
+
+  handleChange(event){
+    console.log(event.target.value)
+    this.setState({templateDirectorySelected: event.target.value})
+    console.log(this.state.templateDirectorySelected);
+  }
+
+  handleChangeSub(event){
+    console.log(event.target.value);
+    this.setState({templateSelected: event.target.value});
+    console.log(this.state.templateSelected);
+  }
+
+  renderFileSelect(){
+    let directory = false;
+    if(this.state.templateDirectorySelected !== 'false'){
+      directory = this.state.templateDirectorySelected;
+    }
+
+    if(directory){
+      const selectedTemplates = this.state.availableTemplateFiles[directory];
+
+      return(
+        <div>
+        Please choose a template.
+        <select value={this.state.templateDirectorySelected} onChange={this.handleChangeSub}>
+          {this.renderListItem(selectedTemplates)}
+        </select>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div id="select_template"><select></select></div>
+      <div id="select_template">
+      Please Select what type of template you would like to complete.
+        <select onChange={this.handleChange}>
+          {this.renderListItem(this.state.availableTemplateDirectories)}
+        </select>
+        {this.renderFileSelect()}
+      </div>
     );
   }
 }
+
 class FormFiller extends Component {
   render() {
     return (
@@ -37,6 +140,11 @@ class DownloadButton extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {templateLoded: false};
+  }
+
   render() {
     return (
       <div className="usa-grid">
